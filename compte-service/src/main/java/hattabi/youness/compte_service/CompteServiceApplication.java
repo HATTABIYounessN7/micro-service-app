29228729d6247dd596c8a406fe1260185bae51a3
HATTABIYounessN7 <youness.hattabi-etu@etu.univh2c.ma>
2026-09-21 17:13:16 +0000

    Init commit

---
+++ compte-service/src/main/java/hattabi/youness/compte_service/CompteServiceApplication.java
@@ -0,0 +1,13 @@
+package hattabi.youness.compte_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompteServiceApplication.class, args);
+	}
+
+}
