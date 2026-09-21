f648cc5a2979b702f4b01ad5971075b399d71b66
HATTABIYounessN7 <youness.hattabi-etu@etu.univh2c.ma>
2026-09-21 19:13:44 +0000

    finalized Part 1 with properties and such

--- compte-service/src/main/java/hattabi/youness/compte_service/CompteServiceApplication.java
+++ compte-service/src/main/java/hattabi/youness/compte_service/CompteServiceApplication.java
@@ -1,7 +1,17 @@
 package hattabi.youness.compte_service;
 
+import java.time.LocalDate;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hattabi.youness.compte_service.entities.Compte;
+import hattabi.youness.compte_service.enums.TypeCompte;
+import hattabi.youness.compte_service.repositories.CompteRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CompteServiceApplication {
@@ -10,4 +20,32 @@ public class CompteServiceApplication {
 		SpringApplication.run(CompteServiceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(CompteRepository compteRepository) {
+		return args -> {
+			List.of(
+					Compte.builder()
+							.solde(10000.0)
+							.dateCreation(LocalDate.now())
+							.type(TypeCompte.COURANT)
+							.build(),
+					Compte.builder()
+							.solde(25000.0)
+							.dateCreation(LocalDate.now())
+							.type(TypeCompte.EPARGNE)
+							.build(),
+					Compte.builder()
+							.solde(5500.0)
+							.dateCreation(LocalDate.now())
+							.type(TypeCompte.COURANT)
+							.build())
+					.forEach(compteRepository::save);
+
+			System.out.println("All Comptes");
+			compteRepository.findAll().forEach(System.out::println);
+
+			System.out.println("Comptes COURANT");
+			compteRepository.findByType(TypeCompte.COURANT).forEach(System.out::println);
+		};
+	}
 }
