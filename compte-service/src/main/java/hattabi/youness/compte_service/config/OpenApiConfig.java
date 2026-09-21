615f04b23e041eb0694d70b0668b54462843c9aa
HATTABIYounessN7 <youness.hattabi-etu@etu.univh2c.ma>
2026-09-21 19:13:09 +0000

    added config for REST repo and for swagger

---
+++ compte-service/src/main/java/hattabi/youness/compte_service/config/OpenApiConfig.java
@@ -0,0 +1,11 @@
+package hattabi.youness.compte_service.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Compte Service", version = "1.0", description = "API pour gestion des comptes bancaires"))
+public class OpenApiConfig {
+
+}
